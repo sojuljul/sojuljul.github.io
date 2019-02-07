4838c4cc1a683fa2145aa7c4f7dd7f5d31102262
sojuljul <nguyenedge@yahoo.com>
2019-02-07 12:11:37 -0800

    Update projects

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
+// Initialize materialize
+document.addEventListener('DOMContentLoaded', function() {
+    var sideNavs = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sideNavs, {});
+
+    var materialBoxes = document.querySelectorAll('.materialboxed');
+    M.Materialbox.init(materialBoxes, {});
+
+    var parallax = document.querySelectorAll('.parallax');
+    M.Parallax.init(parallax, {});
+
+    var tabs = document.querySelectorAll('.tabs');
+    M.Tabs.init(tabs, {});
+
+    var sliders = document.querySelectorAll('.slider');
+    M.Slider.init(sliders, {
+        height: 275
+    });
+
+    var scrollspy = document.querySelectorAll('.scrollspy');
+    M.ScrollSpy.init(scrollspy, {});
+
+});
+
+// scroll back to top
 window.onscroll = function()
 {
     scrollFunction();
@@ -21,13 +46,14 @@ function backTop()
     document.documentElement.scrollTop = 0;
 }
 
-$(document).ready(function(){
-    $(".sidenav").sidenav();
-    $(".materialboxed").materialbox();
-    $(".parallax").parallax();
-    $(".tabs").tabs();
-    $(".slider").slider({
-        height: 275
-    });
-    $(".scrollspy").scrollSpy();
-});
+
+// $(document).ready(function(){
+//     $(".sidenav").sidenav();
+//     $(".materialboxed").materialbox();
+//     $(".parallax").parallax();
+//     $(".tabs").tabs();
+//     $(".slider").slider({
+//         height: 275
+//     });
+//     $(".scrollspy").scrollSpy();
+// });
